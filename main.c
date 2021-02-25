cdd90bff62899e6cb99a35ea1e22395962ef2708
KoSPrograming <prdessauer@miners.utep.edu>
2021-02-25 23:15:48 +0000

    Added main.c file

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 *   Last modified by: Angel F. Garcia (2/19/2021)
 */
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
 
 int main(void) {
-  long x = 10;
-
-  printf("%ld", x);
 
   int opt;
   printf("Enter 1 to test P1, 2 to test P2, 3 to test P3: ");
@@ -25,27 +25,59 @@ int main(void) {
     int x, y;
     scanf("%d %d", &x, &y);
 
-    int x_digit = x / 10;
-    int y_digit = y / 10;
+    int x_digit = x % 100;
+    int y_digit = y % 100;
 
     if(x_digit == y_digit) {
-      print("TRUE");
+      printf("TRUE");
     }
+    else{
 
-    print("FALSE");
-    
+    printf("FALSE");
+    }
     /* Problem 1 End */
   } else if(opt == 2) {
     
     /***************************
      *  Problem 2: Lotto
      ***************************/
-    char winner[5] = "7239";
+    char winner[5] = {'7', '2', '3', '9'};
     char ticket[5];
+    int counter = 0;
 
-    scanf("%s", ticket);
-    
+
     // TODO: Your P2 code goes here
+    
+    scanf("%s", ticket);
+
+    if(ticket[0] == winner[0]){
+      
+      counter++;
+    }
+    
+
+     if(ticket[1] == winner[1])
+    counter++;
+
+     if(ticket[2] == winner[2])
+    counter++;
+
+     if(ticket[3] == winner[3])
+    counter++;
+
+    if(counter == 4){
+      printf("1st Prize!\n");
+    }
+        if(counter == 3){
+      printf("2nd Prize!\n");
+    }
+            if(counter == 2){
+      printf("3rd Prize!\n");
+    }
+            if(counter < 2){
+      printf("No Prize! Better luck next time!\n");
+    }
+
     
     /* Problem 2 End */
   } else if(opt == 3) {
@@ -53,11 +85,53 @@ int main(void) {
     /***************************
      *  Problem 3: Hex2Dec
      ***************************/
-    char hex_in[5];
+    char hex_in[4];
     scanf("%s", hex_in);
 
     //TODO: Your P3 code goes here
+    int number=0;
+    int multiplier;
+    int changer = 0;
     
+    for(int i = 0; i < strlen(hex_in); i++){
+      if (hex_in[changer] == '1')
+        multiplier = 1;
+      else if (hex_in[changer] == '2')
+      multiplier = 2;
+      else if (hex_in[changer] == '3')
+      multiplier = 3;
+      else if (hex_in[changer] == '4')
+      multiplier = 4;
+      else if (hex_in[changer] == '5')
+      multiplier = 5;
+      else if (hex_in[changer] == '6')
+      multiplier = 6;
+      else if (hex_in[changer] == '7')
+      multiplier = 7;
+      else if (hex_in[changer] == '8')
+      multiplier = 8;
+      else if (hex_in[changer] == '9')
+      multiplier = 9;
+      else if (hex_in[changer] == 'a' || hex_in[changer] == 'A')
+      multiplier = 10;
+      else if (hex_in[changer] == 'B' || hex_in[changer] == 'b')
+      multiplier = 11;
+      else if (hex_in[changer] == 'c' || hex_in[changer] == 'C')
+      multiplier = 12;
+      else if (hex_in[changer] == 'd' || hex_in[changer] == 'D')
+      multiplier = 13;
+      else if (hex_in[changer] == 'e' || hex_in[changer] == 'E')
+      multiplier = 14;
+      else if (hex_in[changer] == 'f'|| hex_in[changer] == 'F')
+      multiplier = 15;
+
+        changer++;
+        number = number + pow(16, (strlen(hex_in) - changer)) * multiplier;
+  
+    }
+    printf("%d\n", number);
+
+
     /* Problem 3 End */
   }
 }
